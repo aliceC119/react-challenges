40cb61cc21d55bf64073065e60ca3102062618b7
Alice Chan <alicebay10@yahoo.co.uk>
2024-12-06 08:49:51 +0000

    Commented the useful code for using ES7 Snippets and saved them inside the ES7 Snippets file under the components folder.

---
+++ my-app/src/components/ES7Snippet.js
@@ -0,0 +1,49 @@
+//rfce + enter (This will import React, create the function and export it at the bottom)
+//import React from 'react'
+
+//function ES7Snippet() {
+//  return (
+//    <div>ES7Snippet</div>
+//)
+//)
+//export default ES7Snippet
+
+//rface + enter (This create exact the same as above except it will create and export as an arrow function.)
+//import React from 'react'
+
+//const ES7Snippet = () => {
+//  return (
+//    <div>ES7Snippet</div>
+//  )
+//}
+
+//export default ES7Snippet
+
+//rce + enter (That will create and export a class component.)
+//import React, { Component } from 'react'
+
+//export class ES7Snippet extends Component {
+//  render() {
+//    return (
+//      <div>ES7Snippet</div>
+//    )
+//  }
+//}
+
+//export default ES7Snippet
+
+// rconst +enter (This creates the constructor)
+//constructor(props) {
+//  super(props)
+
+//  this.state = {
+//     
+//  }
+//}
+
+
+//clg + enter (This log something to the console.)
+//console.log(object)
+
+//clo + enter (This log an object along with its name as a string.)
+//console.log('object', object)
